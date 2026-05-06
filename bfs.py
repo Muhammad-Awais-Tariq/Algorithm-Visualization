86de4324b9cd789e887eebfe0fd0cadeec31e9db
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-05-06 19:39:35 +0500

    added bfs with the explored paths

---
+++ bfs.py
@@ -0,0 +1,63 @@
+class node:
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+def actionsequence(graph, goalstate, explored):
+    solution = [goalstate]
+    currentparent = graph[goalstate].parent
+
+    while currentparent is not None:
+        solution.append(currentparent)
+        currentparent = graph[currentparent].parent
+
+    solution.reverse()
+    return solution, explored
+
+def bfs():
+    initialstate = (2, 1) 
+    goalstate = (0, 5)    
+
+    maze = [
+        [1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1]
+    ]
+
+    rows, cols = len(maze), len(maze[0])   
+    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    graph = {}
+
+    for i in range(rows):
+        for j in range(cols):
+            if maze[i][j] == 0:
+                action = []
+                for move in moves:
+                    nr, nc = i + move[0], j + move[1]
+                    if 0 <= nr < rows and 0 <= nc < cols and maze[nr][nc] == 0:
+                        action.append((nr, nc))
+                graph[(i, j)] = node((i, j), None, action)
+
+    frontier = [initialstate]
+    explored = set()
+
+    while frontier:
+        currentnode = frontier.pop(0)
+
+        if currentnode == goalstate:
+            return actionsequence(graph, goalstate, list(explored))
+
+        explored.add(currentnode)
+
+        for child in graph[currentnode].action:
+            if child not in explored and child not in frontier:
+                graph[child].parent = currentnode
+                frontier.append(child)
+
+    return None
+
+print(bfs())
