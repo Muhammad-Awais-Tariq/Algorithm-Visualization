a6bff68d623fec3d695410ea3f11b360d83f835a
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-05-07 19:11:40 +0500

    added dfs as well

--- main.py
+++ main.py
@@ -72,7 +72,45 @@ def bfs(initialstate , goalstate ,maze):
                 frontier.append(child)
 
 
+@tracking
+def dfs(initialstate , goalstate ,maze):
+    rows, cols = len(maze), len(maze[0])   
+    
+    moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
+    graph = {}
+
+    for i in range(rows):
+        for j in range(cols):
+            if maze[i][j] == 0:
+                action = []
+                for move in moves:
+                    nr ,nc = i+move[0] , j+move[1]
+                    if 0 <= nr < rows and 0 <= nc < cols and maze[nr][nc] == 0:
+                        action.append((nr,nc))
+                
+                graph[(i,j)] = node((i,j),None,action)
+
+    frontier = [initialstate]
+    explored = []
+
+    while frontier:
+        currentnode = frontier.pop()
+
+        if graph[currentnode].state == goalstate:
+            return actionsequence(graph,goalstate, list(explored))
+        
+        if currentnode not in explored:
+            explored.append(currentnode)
+
+        for child in graph[currentnode].action:
+
+            if child not in explored and child not in frontier:
+                graph[child].parent = graph[currentnode].state
+
+                if graph[child].state == goalstate:
+                    return actionsequence(graph,goalstate , list(explored))
+                frontier.append(child)
 
 if __name__ == "__main__":
     initialstate = (2, 1) 
@@ -86,3 +124,4 @@ if __name__ == "__main__":
         [1, 1, 1, 1, 1, 1, 1]
     ]
     print(bfs(initialstate ,goalstate ,  maze ))
+    print(dfs(initialstate ,goalstate ,  maze ))
